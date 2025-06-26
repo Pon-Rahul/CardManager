922a427f2d4a8e1fae37737baa3cf9b96f19248e
Pon-Rahul <ponrahul55@gmail.com>
2025-06-26 18:26:29 +0530

    Firebase install

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAZNLooBWCaK4NxH4tLzby29aeCcMDFQM",
+  authDomain: "cardmanager-a3d68.firebaseapp.com",
+  projectId: "cardmanager-a3d68",
+  storageBucket: "cardmanager-a3d68.firebasestorage.app",
+  messagingSenderId: "1038322433246",
+  appId: "1:1038322433246:web:1b1cc2aefb5bcd8c8cd64f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
